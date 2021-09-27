747e0b2679ebc281d33bdbc4ee83ea031ecc9a42
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-09-27 15:22:44 +0100

    HE - Adding transaction history on withdrawal

--- lib/User.rb
+++ lib/User.rb
@@ -6,7 +6,7 @@ class User
 
   def initialize
     @balance = 0.00
-    @transactions = {date: [], withdrawals: [], deposits: [], balance: []}
+    @transactions = { date: [], withdrawals: [], deposits: [], balance: [] }
   end
 
   def deposit(ammount)
@@ -14,11 +14,15 @@ class User
     @transactions[:date].push(DateTime.now.strftime('%d-%m-%Y'))
     @transactions[:withdrawals].push(0.00)
     @transactions[:deposits].push(ammount.to_f)
-    @transactions[:balance].push(@balance)
+    @transactions[:balance] = @balance
   end
 
   def withdraw(ammount)
     @balance -= ammount
+    @transactions[:date].push(DateTime.now.strftime('%d-%m-%Y'))
+    @transactions[:withdrawals].push(ammount.to_f)
+    @transactions[:deposits].push(0.00)
+    @transactions[:balance] = @balance
   end
 
 end
--- spec/User_spec.rb
+++ spec/User_spec.rb
@@ -1,26 +1,22 @@
 require 'User'
 
 describe User do
-
-
+  before(:each) do
+    allow(DateTime).to receive(:now).and_return DateTime.new(2021, 9, 28)
+  end
 
   it{ is_expected.to be_a(User) }
   it{ is_expected.to have_attributes(balance: 0.00)}
 
   describe '#deposit' do
-
-  before(:each) do
-    allow(DateTime).to receive(:now).and_return DateTime.new(2021, 9, 28)
-  end
-
     it 'is expected to add number to balance' do
       subject.deposit(20)
       expect(subject.balance).to eq(20.00)
     end
 
-    it 'is expected to add the activity history to statements' do
+    it 'is expected to add the transaction history to transactions on deposit' do
       subject.deposit(20)
-      expect(subject.transactions).to include(date: ["28-09-2021"], withdrawals: [0.00], deposits: [20.00], balance: [20.00])
+      expect(subject.transactions).to include(date: ["28-09-2021"], withdrawals: [0.00], deposits: [20.00], balance: 20.00)
     end
 
   end
@@ -30,6 +26,12 @@ describe User do
       subject.deposit(20)
       subject.withdraw(5)
       expect(subject.balance).to eq(15.00)
+    end
+
+    it 'is expected to add the transaction history to transactions on withdrawal' do
+      subject.deposit(20)
+      subject.withdraw(20)
+      expect(subject.transactions).to include(date: ["28-09-2021", "28-09-2021"], withdrawals: [0.00, 20.00], deposits: [20.00, 0.00], balance: 0.00)
     end
 
   end
