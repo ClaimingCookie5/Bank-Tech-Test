7c0e53ba0355241152d60864672422264b9819aa
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-09-28 21:01:27 +0100

    HE - Allowing default values for deposit/withdraw in Transaction class

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -1,8 +1,8 @@
 class Transaction
   attr_reader :transaction
 
-  def initialize
-    @transaction = { Date: [], Withdrawals: [], Deposits: [], Balance: [] }
+  def initialize(deposit = 0.00, withdrawal = 0.00)
+    @transaction = { Date: DateTime.now.strftime('%d-%m-%Y'), Withdrawal: withdrawal, Deposit: deposit }
   end
 
 end
--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -1,7 +1,10 @@
 require 'transactions'
 
 describe Transaction do
+  before(:each) do
+    allow(DateTime).to receive(:now).and_return DateTime.new(2021, 9, 28)
+  end
 
-  it { is_expected.to have_attributes( transaction: { Date: [], Withdrawals: [], Deposits: [], Balance: [] }) }
+  it { is_expected.to have_attributes( transaction: { Date: '28-09-2021', Withdrawal: 0.00, Deposit: 0.00}) }
 
 end
