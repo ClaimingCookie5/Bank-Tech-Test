dec28769d973e9d7a02f55e23cc83965c648a5f8
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-09-28 22:15:41 +0100

    HE - Create new instance of self for Transactions class

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -4,11 +4,10 @@ class Transaction
   attr_reader :transaction
 
   def initialize(deposit: deposit = 0.00, withdraw: withdraw = 0.00)
-    @transaction = { Date: DateTime.now.strftime('%d-%m-%Y'), Withdrawal: withdraw, Deposit: deposit }
+    @transaction = { Date: DateTime.now.strftime('%d-%m-%Y'), Withdraw: withdraw, Deposit: deposit }
   end
 
-  def create_new_instance(deposit = 0.00, withdraw = 0.00)
-    Transaction.new(deposit: deposit, withdraw: withdraw)
+  def self.create(deposit = 0.00, withdraw = 0.00)
+    Transaction.new(deposit: deposit, withdraw:  withdraw)
   end
-
 end
--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -5,11 +5,11 @@ describe Transaction do
     allow(DateTime).to receive(:now).and_return DateTime.new(2021, 9, 28)
   end
 
-  it { is_expected.to have_attributes( transaction: { Date: '28-09-2021', Withdrawal: 0.00, Deposit: 0.00}) }
+  it { is_expected.to have_attributes( transaction: { Date: '28-09-2021', Withdraw: 0.00, Deposit: 0.00}) }
 
-  describe '.create_new_instance' do
+  describe '.create_new' do
     it 'is expected to create new instance of Transaction' do
-      expect(subject.create_new_instance).to be_an_instance_of(Transaction)
+      expect(Transaction.create).to be_an_instance_of(Transaction)
     end
   end
 end
