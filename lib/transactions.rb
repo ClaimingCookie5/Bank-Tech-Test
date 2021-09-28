01428a8cfa09a2a776305984d5b082f1ed2327d8
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-09-28 20:50:36 +0100

    HE - Adding attributes to transaction class

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -1,3 +1,8 @@
 class Transaction
+  attr_reader :transaction
+
+  def initialize
+    @transaction = { Date: [], Withdrawals: [], Deposits: [], Balance: [] }
+  end
 
 end
--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -2,4 +2,6 @@ require 'transactions'
 
 describe Transaction do
 
+  it { is_expected.to have_attributes( transaction: { Date: [], Withdrawals: [], Deposits: [], Balance: [] }) }
+
 end
