1424141ef0418eb1cc028884d73b48ce304fcbd6
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-09-28 23:05:55 +0100

    HE - Adding test for depositing amount to transaction

--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -5,11 +5,16 @@ describe Transaction do
     allow(DateTime).to receive(:now).and_return DateTime.new(2021, 9, 28)
   end
 
-  it { is_expected.to have_attributes( transaction: { Date: '28-09-2021', Withdraw: 0.00, Deposit: 0.00}) }
+  it { is_expected.to have_attributes( transaction: { Date: '28-09-2021', Withdraw: 0.00, Deposit: 0.00 }) }
 
   describe '.create_new' do
     it 'is expected to create new instance of Transaction' do
       expect(Transaction.create).to be_an_instance_of(Transaction)
     end
+
+    it 'is expected to add number to deposit' do
+      expect(Transaction.create(10.00)).to have_attributes(transaction: { Date: '28-09-2021', Withdraw: 0.00, Deposit: 10.00 })
+    end
+
   end
 end
