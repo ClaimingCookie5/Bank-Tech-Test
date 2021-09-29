d3bc52f7e617eba8e8f52db67de1432c2aa79c15
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-09-29 01:16:11 +0100

    HE - Adding new create new transaction method to account

--- lib/account.rb
+++ lib/account.rb
@@ -1,8 +1,14 @@
+# frozen_string_literal: true
+
+# Outputs transaction information
 class Account
-  attr_reader :transactions
+  attr_reader :logs
 
   def initialize
-    @transactions = []
+    @logs = []
   end
 
+  def new_transaction(transaction, deposit, withdraw)
+    @logs.push(transaction.create(transaction, deposit, withdraw))
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,7 +1,17 @@
+# frozen_string_literal: true
+
 require 'account'
 
 describe Account do
+  let(:logs) { double('logs', Date: '28-09-2021', Withdraw: 0.00, Deposit: 0.00) }
 
-  it { is_expected.to have_attributes(transactions: []) }
+  it { is_expected.to have_attributes(logs: []) }
 
+  context '.new_transaction' do
+    it 'is expecteed to add trasaction to transactions array' do
+      allow(logs).to receive(:create) { 20.00 }
+      subject.new_transaction(logs, 20.00, 0.00)
+      expect(subject.logs).to contain_exactly(20.00)
+    end
+  end
 end
